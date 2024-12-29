4778b1f5673f46fda743a97780720265e03473a5
Diego Parrales <diegoparrales2005@gmail.com>
2024-12-29 04:35:19 -0500

    IA TERMINADA

--- app/src/main/java/com/example/espol/ed/grupo3/tresenraya/TicTacToe.java
+++ app/src/main/java/com/example/espol/ed/grupo3/tresenraya/TicTacToe.java
@@ -6,27 +6,16 @@ import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-
-
 import com.example.espol.ed.grupo3.tresenraya.Modelo.Computadora;
 import com.example.espol.ed.grupo3.tresenraya.Modelo.Humano;
 import com.example.espol.ed.grupo3.tresenraya.Modelo.Jugador;
 
-import java.util.Random;
 
 public class TicTacToe extends AppCompatActivity {
     private Jugador jugadorActual;
     private Button[][] botones;
     private TextView tvTurno;
-
-
-    private Jugador seleccionarJugador(){
-        Random random = new Random();
-        if (random.nextBoolean()){
-            return new Humano();
-        }
-        return new Computadora();
-    }
+    private char[][] tablero = new char[3][3];
 
     private void configurarBotones(Button boton, int fila, int columna) {
         boton.setOnClickListener(new View.OnClickListener() {
@@ -34,16 +23,44 @@ public class TicTacToe extends AppCompatActivity {
             public void onClick(View v) {
                 if (boton.getText().toString().isEmpty()) {
                     boton.setText(jugadorActual.getTurno());
+                    tablero[fila][columna] = jugadorActual.getTurno().charAt(0);
 
-                    if (verificarVictoria(jugadorActual.getTurno())) {
-                        tvTurno.setText("¡El jugador " + jugadorActual.getTurno() + " ha ganado!");
+                    if (verificarVictoria('O')){
+                        tvTurno.setText("¡El humano ha ganado!");
                         deshabilitarBotones();
-                    } else {
+                    }
+                    else if (esEmpate()){
+                        tvTurno.setText("¡EMPATE!");
+                        deshabilitarBotones();
+                    } else{
                         cambiarTurno();
+                        turnoComputadora();
                     }
                 }
             }
         });
+    }
+
+    private void turnoComputadora(){
+        if (verificarVictoria('O') || verificarVictoria('X') || esEmpate()) {
+            return;
+        }
+
+        int[] mejorMovimiento = calcularMejorMovimiento();
+        if (mejorMovimiento[0] != -1 && mejorMovimiento[1] != -1){
+            botones[mejorMovimiento[0]][mejorMovimiento[1]].setText("X");
+            tablero[mejorMovimiento[0]][mejorMovimiento[1]] = 'X';
+
+            if (verificarVictoria('X')){
+                tvTurno.setText("¡La computadora ha ganado!");
+                deshabilitarBotones();
+            } else if (esEmpate()){
+                tvTurno.setText("¡EMPATE!");
+                deshabilitarBotones();
+            } else{
+                cambiarTurno();
+            }
+        }
     }
 
     private void deshabilitarBotones(){
@@ -54,7 +71,6 @@ public class TicTacToe extends AppCompatActivity {
         }
     }
 
-
     private void cambiarTurno(){
         if (jugadorActual instanceof Humano){
             jugadorActual = new Computadora();
@@ -64,50 +80,122 @@ public class TicTacToe extends AppCompatActivity {
         tvTurno.setText("Turno de " + jugadorActual.getTurno());
     }
 
-    private boolean verificarVictoria(String turno) {
+    private boolean verificarVictoria(char turno) {
         for (int i = 0; i < 3; i++) {
-            if (botones[i][0].getText().toString().equals(turno) &&
-                    botones[i][1].getText().toString().equals(turno) &&
-                    botones[i][2].getText().toString().equals(turno)) {
-                return true;
-            }
+            if (tablero[i][0] == turno && tablero[i][1] == turno && tablero[i][2] == turno) return true;
         }
 
         for (int j = 0; j < 3; j++) {
-            if (botones[0][j].getText().toString().equals(turno) &&
-                    botones[1][j].getText().toString().equals(turno) &&
-                    botones[2][j].getText().toString().equals(turno)) {
-                return true;
-            }
+            if (tablero[0][j] == turno && tablero[1][j] == turno && tablero[2][j] == turno) return true;
         }
 
-        if (botones[0][0].getText().toString().equals(turno) &&
-                botones[1][1].getText().toString().equals(turno) &&
-                botones[2][2].getText().toString().equals(turno)) {
-            return true;
-        }
-
-        if (botones[0][2].getText().toString().equals(turno) &&
-                botones[1][1].getText().toString().equals(turno) &&
-                botones[2][0].getText().toString().equals(turno)) {
-            return true;
-        }
+        if (tablero[0][0] == turno && tablero[1][1] == turno && tablero[2][2] == turno) return true;
+        if (tablero[0][2] == turno && tablero[1][1] == turno && tablero[2][0] == turno) return true;
 
         return false;
     }
 
+
+    public int[] calcularMejorMovimiento() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (tablero[i][j] == ' ') {
+                    tablero[i][j] = 'O';
+                    if (verificarVictoria('O')) {
+                        tablero[i][j] = ' ';
+                        return new int[]{i, j};
+                    }
+                    tablero[i][j] = ' ';
+                }
+            }
+        }
+
+        int mejorValor = Integer.MIN_VALUE;
+        int[] mejorMovimiento = {-1, -1};
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (tablero[i][j] == ' ') {
+                    tablero[i][j] = 'X';
+                    int valorMovimiento = minimax(false);
+                    tablero[i][j] = ' ';
+
+                    if (valorMovimiento > mejorValor) {
+                        mejorValor = valorMovimiento;
+                        mejorMovimiento[0] = i;
+                        mejorMovimiento[1] = j;
+                    }
+                }
+            }
+        }
+        return mejorMovimiento;
+    }
+
+
+    private void inicializarTablero() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                tablero[i][j] = ' ';
+            }
+        }
+    }
+
+    private int minimax(boolean esTurnoHumano) {
+        if (verificarVictoria('X')) return 10;
+        if (verificarVictoria('O')) return -10;
+        if (esEmpate()) return 0;
+
+        if (esTurnoHumano) {
+            int mejorValor = Integer.MAX_VALUE;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (tablero[i][j] == ' ') {
+                        tablero[i][j] = 'O'; // Simula turno humano
+                        mejorValor = Math.min(mejorValor, minimax(false));
+                        tablero[i][j] = ' '; // Deshacer movimiento
+                    }
+                }
+            }
+            return mejorValor;
+        } else {
+            int mejorValor = Integer.MIN_VALUE;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (tablero[i][j] == ' ') {
+                        tablero[i][j] = 'X'; // Simula turno computadora
+                        mejorValor = Math.max(mejorValor, minimax(true));
+                        tablero[i][j] = ' '; // Deshacer movimiento
+                    }
+                }
+            }
+            return mejorValor;
+        }
+    }
+
+
+    private boolean esEmpate(){
+        for (int i = 0; i < 3; i++) {
+            for (int j=0; j < 3; j++){
+                if (botones[i][j].getText().toString().isEmpty()){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tic_tac_toe);
 
-        jugadorActual = seleccionarJugador();
+        jugadorActual = new Humano();
         tvTurno = findViewById(R.id.tv_turn);
         GridLayout gridLayout = findViewById(R.id.gridLayout);
 
         tvTurno.setText("Turno de " + jugadorActual.getTurno());
         botones = new Button[3][3];
+        inicializarTablero();
 
         for (int i = 0; i < 3; i++){
             for (int j = 0; j < 3; j++){
@@ -118,8 +206,8 @@ public class TicTacToe extends AppCompatActivity {
             }
         }
     }
-
-
-
-
 }
+
+
+
+
